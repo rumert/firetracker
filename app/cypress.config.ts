3ddac6b3fbf3db58ac0678bcf0611e8419d6ce25
rumert <rumertkiran@gmail.com>
2024-08-17 17:42:51 +0300

    fix attempt

--- app/cypress.config.ts
+++ app/cypress.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     baseUrl: "http://localhost:3000",
   },
   env: {
-    MAIN_API_URL: 'http://api:4000',
-    AUTH_API_URL: 'http://api:5000',
+    MAIN_API_URL: 'http://firetracker-api:4000',
+    AUTH_API_URL: 'http://firetracker-api:5000',
   },
 });

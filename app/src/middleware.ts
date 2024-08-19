4688d93c2e1b31cbcb4e084c4f629639b01dcda1
rumert <rumertkiran@gmail.com>
2024-08-19 22:24:28 +0300

    added logs to determine issue

--- app/src/middleware.ts
+++ app/src/middleware.ts
@@ -2,6 +2,8 @@ import { NextRequest, NextResponse } from "next/server";
 
 export default async function middleware(req: NextRequest): Promise<NextResponse> {
   const refreshToken = req.cookies.get('refreshToken')?.value;
+  console.log('Middleware called, pathname:', req.nextUrl.pathname);
+  console.log('Refresh token:', refreshToken);
 
   if (!refreshToken) {
     console.log('no refresh token')
@@ -47,6 +49,8 @@ async function handleAuthenticatedRequest(req: NextRequest, refreshToken: string
     httpOnly: true, 
     secure: false,
     sameSite: 'lax',
+    path: '/',
+    domain: 'ec2-13-60-69-222.eu-north-1.compute.amazonaws.com',
   });
   
   return response;

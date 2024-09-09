87f212cb2de98f9aaead5c2587242c9153369d47
rumert <rumertkiran@gmail.com>
2024-09-09 19:56:56 +0300

    redis container
    redis is running on a container instead of using redis cloud to reduce delay

--- api/src/config/redis.js
+++ api/src/config/redis.js
@@ -1,16 +1,15 @@
 const Redis = require("redis");
 const redisClient = Redis.createClient({
-    password: process.env.REDIS_PASSWORD,
     socket: {
-        host: process.env.REDIS_HOST,
-        port: 14639
+        host: process.env.REDIS_HOST || 'redis',
+        port: process.env.REDIS_PORT || 6379
     }
 });
 
 redisClient.connect();
 
 redisClient.on('error', (err) => {
-    //console.log("redis error: ", err);
+    console.log("redis error: ", err);
 });
 
 module.exports = redisClient;

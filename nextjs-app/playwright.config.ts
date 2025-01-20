9a58f90de966ffb7cb2f28cc333ce840b86cd627
rumert <rumertkiran@gmail.com>
2025-01-20 16:56:36 +0300

    minor changes

--- nextjs-app/playwright.config.ts
+++ nextjs-app/playwright.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    baseURL: 'http://localhost:3000',
+    baseURL: process.env.APP_URL,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',

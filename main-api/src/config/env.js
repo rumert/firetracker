8f7ef9f2212ebd10e0a0b515c833a1c929b67894
rumert <rumertkiran@gmail.com>
2025-02-01 19:27:12 +0300

    debug

--- main-api/src/config/env.js
+++ main-api/src/config/env.js
@@ -2,6 +2,17 @@ const dotenv = require('dotenv')
 const path = require('path')
 dotenv.config({ path: path.resolve(__dirname, '..', `.env.${process.env.NODE_ENV}`) });
 
+console.log(
+    process.env.NODE_ENV,
+    process.env.ACCESS_TOKEN_SECRET,
+    process.env.REFRESH_TOKEN_SECRET,
+    process.env.MONGO_URL,
+    process.env.REDIS_HOST,
+    process.env.REDIS_PORT,
+    process.env.REDIS_DB,
+    process.env.AI_KEY,
+)
+
 module.exports={
     NODE_ENV: process.env.NODE_ENV,
     ACCESS_TOKEN: process.env.ACCESS_TOKEN_SECRET,
